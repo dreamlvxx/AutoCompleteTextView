09920b5e62ca3752dab5b619736ad741890eac84
xingxinglv <lvxingxing@kingsoft.com>
2020-06-05 15:22:32 +0800

    init repo

---
+++ app/src/main/java/com/dream/autocompletetextview/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.dream.autocompletetextview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.util.Rfc822Tokenizer
+import android.widget.ArrayAdapter
+import android.widget.MultiAutoCompleteTextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    val data = arrayOf("lvsadf@163.com", "xxx@126.com", "ccc@kingsoft.com", "asdsad@163.com", "uuu")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, data)
+        actv_content.setAdapter(adapter)
+        val t = MyTokenizer(';')
+        actv_content.setTokenizer(t)
+    }
+}
